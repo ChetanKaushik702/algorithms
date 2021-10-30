ee0315d705ce350edfe5d137f656dddb7276f92a
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-10-30 20:08:39 +0530

    Travelling Salesman implementation done usin DP and bitmask

--- implementations/TSP.java
+++ implementations/TSP.java
@@ -1,65 +1,33 @@
 import java.util.*;
 
 public class TSP {
-    int N;
-    int dp[][];
-    int cities[][];
 
-    TSP(int N) {
-        this.N = N;
-        dp = new int[1 << N][N];
-        cities = new int[N][N];
-    }
-
-    int tsp (int mask, int pos, int src) {
-        if (mask == (1 << N) - 1) {
-            return cities[pos][src];
-        }
-        if (dp[mask][pos] != -1) return dp[mask][pos];
+    static int tsp(int[][] dist, int mask, int pos, int n, int[][] dp) {
+        if (mask == ((1 << n) - 1))  return dist[pos][0];
+        if (dp[mask][pos] != 0) return dp[mask][pos];
         int ans = Integer.MAX_VALUE;
-        for (int city = 0; city < N; city++) {
+        for (int city = 0; city < n; city++) {
             if ((mask & (1 << city)) == 0) {
-                int newCost = cities[pos][city] + tsp(mask | (1 << city), city, src);
-                ans = Math.min(ans, newCost);
+                int newAns = dist[pos][city] + tsp(dist, mask | (1 << pos), city, n, dp);
+                ans = Math.min(ans, newAns);
             }
         }
         dp[mask][pos] = ans;
         return dp[mask][pos];
     }
-    
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        TSP tspObj = new TSP(N);
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < tspObj.N; j++) {
-                tspObj.dp[i][j] = -1;
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int[][] dist = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    dist[i][j] = sc.nextInt();
+                }
             }
-        }
-        for (int i = 0; i < N; i++) {
-            System.out.println("Enter cost of city " + (i+1) + " to all cities"); 
-            for (int j = 0; j < N; j++) {
-                tspObj.cities[i][j] = sc.nextInt();
-            }
-        }
-
-        // int ans = Integer.MAX_VALUE;
-        // for (int i = 0; i < N; i++) {
-        //     tspObj.printDP();
-        //     ans = Math.min(ans, tspObj.tsp(1 << i, i, i));
-        //     System.out.println("Minimum weight of Hamiltonian cycle starting at vertex " + (i + 1) + " is: " + ans);
-        // }
-        int ans = tspObj.tsp(1, 0, 0);
-        System.out.println("Overall minimum weight of Hamiltonian cycle:" + ans);
-        sc.close();
-    }
-
-    void printDP() {
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(dp[i][j] + " ");
-            }
-            System.out.println();
+            int[][] dp = new int[1 << n][n];
+            int minPathLen = tsp(dist, 0, 0, n, dp);
+            System.out.println(minPathLen);
         }
     }
 }
